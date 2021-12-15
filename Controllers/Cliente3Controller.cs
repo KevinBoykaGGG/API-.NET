2051d50f1030e8919bcf16ec159ad4a9a488afe2
Kevin Najera <kevinalex360boyka@gmail.com>
2021-12-14 18:06:58 -0600

    api version 1

---
+++ Controllers/Cliente3Controller.cs
@@ -0,0 +1,41 @@
+﻿using ClassLibrary1.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.API.Controllers
+{
+    //en api se suele usar get, post, put, delete ....//
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Cliente3Controller : ControllerBase
+    {
+        private readonly IClienteRepository _clienteRepository;
+
+        public Cliente3Controller(IClienteRepository clienteRepository)
+        {
+            _clienteRepository = clienteRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Clientes()
+        {
+            var clientes = await _clienteRepository.GetClientes();
+            return Ok(clientes);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Cliente(int id)
+        {
+            var cliente = await _clienteRepository.GetClienteById(id);
+            return Ok(cliente);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Clientes(Cliente cliente)
+        {
+            await _clienteRepository.InsertCliente(cliente);
+            return Ok("");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+using ClassLibrary1.Interfaces;
+using ClassLibrary1.Repository;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<CLContext>(options =>
+  options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
+
+builder.Services.AddScoped<IClienteRepository, ClienteRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
